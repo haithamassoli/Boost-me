06ded7c6007dbdd1fd4d8a7e2a89ba1e95a32aca
haithamassoli <haitham.b.assoli@gmail.com>
2022-03-13 16:32:50 +0300

    styling slider

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -19,107 +19,104 @@ function Home() {
           />
         </h3>
       </div>
-
-      <Swiper
-        effect="coverflow"
-        grabCursor="true"
-        centeredSlides="true"
-        slidesPerView={
-          window.innerWidth > 1000 ? 4 : window.innerWidth > 600 ? 3 : 2
-        }
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        loop={true}
-        autoplay={{
-          delay: 2500,
-        }}
-        coverflowEffect={{
-          rotate: 5, // slide rotation degree
-          stretch: 20, // stretches the space between the slides in px
-          depth: 200, // offsets the depth of neighboring slides
-          modifier: 1, // effe ct multiplier
-          slideShadows: true, // disables the shadow around the slide container
-        }}
-        breakpoints={{
-          700: {
-            spaceBetween: 0,
-            slidesPerView: 4,
-          },
-          500: {
-            spaceBetween: 100,
-            slidesPerView: 2,
-          },
-          411: {
-            spaceBetween: 100,
-            slidesPerView: 2,
-          },
-          300: {
-            spaceBetween: 0,
-            slidesPerView: 1,
-          },
-        }}
-        modules={[Pagination, Navigation, Autoplay, EffectCoverflow]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <img
-            className=" h-full w-full bg-black/50"
-            src="https://cdn1.epicgames.com/offer/b7b42e2078524ab386a8b2a9856ef557/EGS_SIFUStandardEdition_Sloclap_S4_1200x1600-32aca69d756abfcc25f8581942a6162b_1200x1600-32aca69d756abfcc25f8581942a6162b?h=854&resize=1&w=640"
-            alt="SIFU"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://cdn1.epicgames.com/offer/b7b42e2078524ab386a8b2a9856ef557/EGS_SIFUStandardEdition_Sloclap_S4_1200x1600-32aca69d756abfcc25f8581942a6162b_1200x1600-32aca69d756abfcc25f8581942a6162b?h=854&resize=1&w=640"
-            alt="SIFU"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://cdn1.epicgames.com/offer/b7b42e2078524ab386a8b2a9856ef557/EGS_SIFUStandardEdition_Sloclap_S4_1200x1600-32aca69d756abfcc25f8581942a6162b_1200x1600-32aca69d756abfcc25f8581942a6162b?h=854&resize=1&w=640"
-            alt="SIFU"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://cdn1.epicgames.com/offer/b7b42e2078524ab386a8b2a9856ef557/EGS_SIFUStandardEdition_Sloclap_S4_1200x1600-32aca69d756abfcc25f8581942a6162b_1200x1600-32aca69d756abfcc25f8581942a6162b?h=854&resize=1&w=640"
-            alt="SIFU"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://cdn1.epicgames.com/offer/b7b42e2078524ab386a8b2a9856ef557/EGS_SIFUStandardEdition_Sloclap_S4_1200x1600-32aca69d756abfcc25f8581942a6162b_1200x1600-32aca69d756abfcc25f8581942a6162b?h=854&resize=1&w=640"
-            alt="SIFU"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://cdn1.epicgames.com/offer/b7b42e2078524ab386a8b2a9856ef557/EGS_SIFUStandardEdition_Sloclap_S4_1200x1600-32aca69d756abfcc25f8581942a6162b_1200x1600-32aca69d756abfcc25f8581942a6162b?h=854&resize=1&w=640"
-            alt="SIFU"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://cdn1.epicgames.com/offer/b7b42e2078524ab386a8b2a9856ef557/EGS_SIFUStandardEdition_Sloclap_S4_1200x1600-32aca69d756abfcc25f8581942a6162b_1200x1600-32aca69d756abfcc25f8581942a6162b?h=854&resize=1&w=640"
-            alt="SIFU"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://cdn1.epicgames.com/offer/b7b42e2078524ab386a8b2a9856ef557/EGS_SIFUStandardEdition_Sloclap_S4_1200x1600-32aca69d756abfcc25f8581942a6162b_1200x1600-32aca69d756abfcc25f8581942a6162b?h=854&resize=1&w=640"
-            alt="SIFU"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://cdn1.epicgames.com/offer/b7b42e2078524ab386a8b2a9856ef557/EGS_SIFUStandardEdition_Sloclap_S4_1200x1600-32aca69d756abfcc25f8581942a6162b_1200x1600-32aca69d756abfcc25f8581942a6162b?h=854&resize=1&w=640"
-            alt="SIFU"
-          />
-        </SwiperSlide>
-      </Swiper>
+      <div className="  bg-black">
+        <Swiper
+          // effect="coverflow"
+          // slidesOffsetAfter={20}
+          // allowSlideNext={true}
+          grabCursor="true"
+          centeredSlides="true"
+          slidesPerView={2}
+          spaceBetween={10}
+          pagination={{
+            // el: ".customized-swiper-pagination",
+            clickable: true,
+          }}
+          navigation={true}
+          loop={true}
+          autoplay={{
+            delay: 2500,
+          }}
+          // coverflowEffect={{
+          //   rotate: 0, // slide rotation degree
+          //   stretch: 0, // stretches the space between the slides in px
+          //   depth: 0, // offsets the depth of neighboring slides
+          //   modifier: 1, // effect multiplier
+          //   slideShadows: false, // disables the shadow around the slide container
+          // }}
+          breakpoints={{
+            1000: {
+              slidesPerView: 4,
+            },
+            600: {
+              slidesPerView: 3,
+            },
+          }}
+          modules={[Pagination, Navigation, Autoplay, EffectCoverflow]}
+          className="mySwiper pb-10"
+        >
+          <SwiperSlide>
+            <img
+              src="https://cdn1.epicgames.com/offer/b7b42e2078524ab386a8b2a9856ef557/EGS_SIFUStandardEdition_Sloclap_S4_1200x1600-32aca69d756abfcc25f8581942a6162b_1200x1600-32aca69d756abfcc25f8581942a6162b?h=854&resize=1&w=640"
+              alt="SIFU"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://s3.gaming-cdn.com/images/products/6878/orig/jogo-steam-yakuza-like-a-dragon-cover.jpg"
+              alt="SIFU"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://cdn1.epicgames.com/offer/b7b42e2078524ab386a8b2a9856ef557/EGS_SIFUStandardEdition_Sloclap_S4_1200x1600-32aca69d756abfcc25f8581942a6162b_1200x1600-32aca69d756abfcc25f8581942a6162b?h=854&resize=1&w=640"
+              alt="SIFU"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://cdn1.epicgames.com/offer/b7b42e2078524ab386a8b2a9856ef557/EGS_SIFUStandardEdition_Sloclap_S4_1200x1600-32aca69d756abfcc25f8581942a6162b_1200x1600-32aca69d756abfcc25f8581942a6162b?h=854&resize=1&w=640"
+              alt="SIFU"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://cdn1.epicgames.com/offer/b7b42e2078524ab386a8b2a9856ef557/EGS_SIFUStandardEdition_Sloclap_S4_1200x1600-32aca69d756abfcc25f8581942a6162b_1200x1600-32aca69d756abfcc25f8581942a6162b?h=854&resize=1&w=640"
+              alt="SIFU"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://cdn1.epicgames.com/offer/b7b42e2078524ab386a8b2a9856ef557/EGS_SIFUStandardEdition_Sloclap_S4_1200x1600-32aca69d756abfcc25f8581942a6162b_1200x1600-32aca69d756abfcc25f8581942a6162b?h=854&resize=1&w=640"
+              alt="SIFU"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://cdn1.epicgames.com/offer/b7b42e2078524ab386a8b2a9856ef557/EGS_SIFUStandardEdition_Sloclap_S4_1200x1600-32aca69d756abfcc25f8581942a6162b_1200x1600-32aca69d756abfcc25f8581942a6162b?h=854&resize=1&w=640"
+              alt="SIFU"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://cdn1.epicgames.com/offer/b7b42e2078524ab386a8b2a9856ef557/EGS_SIFUStandardEdition_Sloclap_S4_1200x1600-32aca69d756abfcc25f8581942a6162b_1200x1600-32aca69d756abfcc25f8581942a6162b?h=854&resize=1&w=640"
+              alt="SIFU"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://cdn1.epicgames.com/offer/b7b42e2078524ab386a8b2a9856ef557/EGS_SIFUStandardEdition_Sloclap_S4_1200x1600-32aca69d756abfcc25f8581942a6162b_1200x1600-32aca69d756abfcc25f8581942a6162b?h=854&resize=1&w=640"
+              alt="SIFU"
+            />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+        explicabo aliquid quas dicta nemo vel. Tenetur, laboriosam at optio
+        blanditiis voluptate tempora culpa? Voluptates incidunt numquam
+        inventore animi, neque eaque!
+      </div>
     </>
   );
 }
