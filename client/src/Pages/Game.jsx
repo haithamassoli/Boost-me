37a65f6e7bfd9bf09e36b979558a9837f20f8316
haithamassoli <haitham.b.assoli@gmail.com>
2022-03-16 10:11:19 +0300

    styling single page

--- client/src/Pages/Game.jsx
+++ client/src/Pages/Game.jsx
@@ -11,7 +11,7 @@ function Game() {
   return (
     <div className="mt-10 px-10">
       <h1 className="mb-10 text-4xl">Assassin's Creed® Valhalla</h1>
-      <div className="grid grid-cols-4 gap-5">
+      <div className="grid grid-cols-5 justify-around gap-5">
         <Swiper
           // effect="coverflow"
           // slidesOffsetAfter={20}
@@ -30,7 +30,7 @@ function Game() {
             delay: 2500,
           }}
           modules={[Pagination, Navigation, Autoplay, EffectCoverflow]}
-          className=" col-span-3"
+          className="col-span-5 md:col-span-3"
         >
           <SwiperSlide>
             <img
@@ -102,7 +102,7 @@ function Game() {
             />
           </SwiperSlide>
         </Swiper>
-        <div>
+        <div className="col-span-5 m-10 md:col-span-2 md:m-0 lg:m-5 xl:m-10">
           <img
             src="https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Fassassins-creed-valhalla%2Fhome%2FAC-KINGDOM-PREORDER_STANDARD-EDITION_EPIC_Game_Logo_whiteblue_1000x375-1000x375-baa332e9500ef71697f4a31924488fe309c4784d.png?h=270&resize=1&w=480"
             alt="game"
@@ -115,32 +115,126 @@ function Game() {
             <div className="font-semibold">$23.99</div>
           </div>
           <p>Sale ends 3/17/2022 at 6:00 PM</p>
-          <button className="mt-3 w-full rounded-lg bg-green-300 px-4 py-2 font-semibold text-black">
-            BOOST ME
-          </button>
+
           <div className="flex justify-center ">
-            <div className="mb-3 xl:w-96">
-              <select
-                className="form-select focus:text-white-700 mt-5
-      block
-      w-full
-      appearance-none
-      rounded-lg
-      border
-      border-solid
-      bg-black bg-clip-padding bg-no-repeat
-      px-4 py-2
-      font-normal
-      text-white
-      transition
-      ease-in-out focus:border-green-300 focus:bg-black focus:outline-none"
-                aria-label="Default select example"
-              >
-                <option selected>Open this select menu</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-              </select>
+            <div className="mb-3 w-full">
+              <div>
+                <div
+                  className="relative mt-5 flex w-full items-center justify-evenly
+                            rounded-lg
+                            border-2
+                            border-solid
+                            border-[#232b46]
+                            after:absolute after:top-1 after:left-[75px]
+                            after:text-[15px]
+                            after:text-[#979aae] after:content-['CurrentDivision']"
+                >
+                  <img
+                    className="h-[60px] border-r-2 border-[#232b46] object-contain p-2"
+                    src="https://eloking.com/img/icons/valorant/ranks/matchmaking/18@2x.png"
+                    alt=""
+                  />
+                  <select
+                    className="form-select focus:text-white-700 w-full cursor-pointer bg-[#080F28] px-4 py-5 pb-2 font-normal
+                            text-white
+                            focus:border-green-300 focus:outline-none"
+                  >
+                    <option value="1">Bronze |</option>
+                    <option value="2">Bronze ||</option>
+                    <option value="3">Bronze |||</option>
+                    <option value="3">Silver |</option>
+                    <option value="3">Silver ||</option>
+                    <option value="3">Silver |||</option>
+                    <option value="3">Gold |</option>
+                    <option value="3">Gold ||</option>
+                    <option value="3">Gold |||</option>
+                  </select>
+                </div>
+                <div
+                  className="mt-5 flex w-full items-center justify-evenly rounded-lg
+                            border-2
+                            border-solid
+                            border-[#232b46]"
+                >
+                  <img
+                    className="h-[60px] border-r-2 border-[#232b46] object-contain p-2"
+                    src="https://eloking.com/img/icons/valorant/ranks/matchmaking/18@2x.png"
+                    alt=""
+                  />
+                  <select
+                    className="form-select focus:text-white-700 w-full 
+                            cursor-pointer
+                          bg-[#080F28]
+                            px-4 py-2
+                            font-normal
+                            text-white
+                            focus:border-green-300 focus:outline-none"
+                  >
+                    <option value="1">Bronze |</option>
+                    <option value="2">Bronze ||</option>
+                    <option value="3">Bronze |||</option>
+                    <option value="3">Silver |</option>
+                    <option value="3">Silver ||</option>
+                    <option value="3">Silver |||</option>
+                    <option value="3">Gold |</option>
+                    <option value="3">Gold ||</option>
+                    <option value="3">Gold |||</option>
+                  </select>
+                </div>
+              </div>
+              <div className="mt-5 grid grid-cols-1 items-center justify-center gap-3 lg:grid-cols-2">
+                <label
+                  className="form-check-label group relative inline-block text-white after:absolute after:top-0 after:right-0 after:flex after:h-5 after:w-1 after:items-center after:justify-center after:rounded-tr-md after:rounded-bl-md after:border-2 after:border-green-500/20 after:bg-[#3FB9BE] after:px-2 after:text-[13px] after:font-bold after:text-black after:content-['i']"
+                  for="inlineCheckbox1"
+                >
+                  <div className="form-check form-check-inline cursor-pointer rounded-lg border border-gray-600 bg-slate-800 px-2 py-2">
+                    <input
+                      className="form-check-input float-left mt-1 mr-2 h-4 w-4 cursor-pointer appearance-none rounded-sm border border-gray-300 bg-black bg-contain bg-center bg-no-repeat align-top transition duration-200 checked:border-green-300 checked:bg-green-300 focus:outline-none"
+                      type="checkbox"
+                      id="inlineCheckbox1"
+                      value="option1"
+                    />
+                    Live Stream
+                  </div>
+                </label>
+                <label
+                  className="form-check-label group relative inline-block text-white after:absolute after:top-0 after:right-0 after:flex after:h-5 after:w-1 after:items-center after:justify-center after:rounded-tr-md after:rounded-bl-md after:border-2 after:border-green-500/20 after:bg-[#3FB9BE] after:px-2 after:text-[13px] after:font-bold after:text-black after:content-['i']"
+                  for="inlineCheckbox2"
+                >
+                  <div className="form-check form-check-inline cursor-pointer rounded-lg border border-gray-600 bg-slate-800 px-2 py-2">
+                    <input
+                      className="form-check-input float-left mt-1 mr-2 h-4 w-4 cursor-pointer appearance-none rounded-sm border border-gray-300 bg-black bg-contain bg-center bg-no-repeat align-top transition duration-200 checked:border-green-300 checked:bg-green-300 focus:outline-none"
+                      type="checkbox"
+                      id="inlineCheckbox2"
+                      value="option2"
+                    />
+                    Appear Online
+                    <span className="ml-3 rounded-xl bg-blue-500 px-2 font-semibold">
+                      Free
+                    </span>
+                  </div>
+                </label>
+                <label
+                  className="form-check-label group relative flex items-center justify-center  text-sm text-white after:absolute after:top-0 after:right-0 after:flex after:h-5 after:w-1 after:items-center after:justify-center after:rounded-tr-md after:rounded-bl-md after:border-2 after:border-green-500/20 after:bg-[#3FB9BE] after:px-2 after:text-[13px] after:font-bold after:text-black after:content-['i']"
+                  for="inlineCheckbox3"
+                >
+                  <div className="form-check  form-check-inline cursor-pointer rounded-lg border border-gray-600 bg-slate-800 px-2 py-2">
+                    <input
+                      className="form-check-input float-left mr-2 mt-[2px] h-4 w-4 cursor-pointer appearance-none rounded-sm border border-gray-300 bg-black bg-contain bg-center bg-no-repeat align-top transition duration-200 checked:border-green-300 checked:bg-green-300 focus:outline-none"
+                      type="checkbox"
+                      id="inlineCheckbox3"
+                      value="option2"
+                    />
+                    Play with Booster
+                    <span className="ml-3 rounded-xl bg-red-500 px-2 text-sm font-semibold">
+                      Hot
+                    </span>
+                  </div>
+                </label>
+              </div>
+              <button className="mt-3 w-full rounded-lg bg-green-300 px-4 py-2 font-semibold text-black">
+                BOOST ME
+              </button>
             </div>
           </div>
         </div>
